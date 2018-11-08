f1aac11de9161a82a4caf56680d259f903036e95
Adarsh Srivastava <adarshs@iitk.ac.in>
2018-11-08 19:58:48 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const fs = require('fs');
+const solc = require('solc')
+const Web3 = require('web3');
+const web3 = new Web3();
+
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+const address = web3.eth.accounts[0];
+
+const code = fs.readFileSync('Voting.sol').toString()
+const compiledCode = solc.compile(code)
+
+const abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
+console.log(compiledCode.contracts[':Voting'].interface)
+const byteCode = compiledCode.contracts[':Voting'].bytecode
+
+const VotingContract = web3.eth.contract(abiDefinition)
+const deployedContract = VotingContract.new(['Rama','Nick','Jose'], {
+  data: byteCode,
+  from: web3.eth.accounts[0], gas: 4712388
+}, (err, contract) => {
+});
